746000ed46026963ae2936c68e84916980d5a515
Aio-Dev-JS <AioDev.JS@gmail.com>
2020-09-12 20:52:32 +0900

    Lecture 2.1 DevServer, Watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
 import del from "del";
 import gulp from "gulp";
 import gpug from "gulp-pug"
+import ws from "gulp-webserver"
 
 const routes = {
     pug: {
+        watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
     }
@@ -14,11 +16,20 @@ const pug = () => gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pu
 
 const clean = () => del(["build"]);
 
+const webserver = () => gulp.src("build").pipe(ws({ livereload: true, open: true }));
+
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug)
+}
+// 
+
 const prepare = gulp.series([clean])
 
 const assets = gulp.series([pug])
 
-export const dev = gulp.series([prepare, assets]);
+const postDev = gulp.parallel([webserver, watch])
+
+export const dev = gulp.series([prepare, assets, postDev]);
 //  export는 package.json에서 쓸 함수에만 적용해주면 됨
 // const gulp = require("gulp"); 
 // 구 자바스크립트
