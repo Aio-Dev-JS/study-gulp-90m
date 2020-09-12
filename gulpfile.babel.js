159d059a55a64083264ba0759ea2e83a790cbeba
Aio-Dev-JS <AioDev.JS@gmail.com>
2020-09-12 20:15:34 +0900

    Lecture 0.5

---
+++ gulpfile.babel.js
@@ -0,0 +1,6 @@
+import gulp from "gulp";
+
+export const dev = () => console.log("I will dev");
+
+// const gulp = require("gulp"); 
+// 구 자바스크립트
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import { random } from "./util"
+
+const r0one = random(10)
+const rTwo = random(20)
+
+console.log(`${r0one} ${rTwo}`)
